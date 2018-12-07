ca8ebd86ae050e54860cb916ed34b7007d8ec1eb
Kevin Herrera <kevin@herrera.io>
2018-12-06 16:18:18 -0800

    Creating aliases for ls.

---
+++ utils/ls.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+###
+# Generates an `ls` command with the desired options.
+#
+# This function will iterate through the desired options to check if the
+# available `ls` command supports them. If the option is listed in the help
+# message, it will be included with the output for the `ls` command.
+##
+function bu_ls_command
+{
+    COMMAND="ls"
+    HELP="$(ls --help)"
+    OPTIONS=(
+        --color=auto
+        --group-directories-first
+        --hide-control-chars
+        -l
+    )
+
+    for OPTION in "${OPTIONS[@]}"; do
+        if [[ $HELP == *$OPTION* ]]; then
+            COMMAND="$COMMAND $OPTION"
+        fi
+    done
+
+    echo "$COMMAND"
+}
+
+# Create an alias with the desired options.
+alias ll="$(bu_ls_command)"
+
+# Create an alias to display hidden files.
+alias la='ll -a'
+
+# Create an alias to display human friendly file sizes.
+alias lh='ll -h'
