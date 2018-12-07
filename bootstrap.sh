bba4ed43746a70ecaa1650be69fd4436388166f0
Kevin Herrera <kevin@herrera.io>
2018-12-06 16:17:54 -0800

    Creating the bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+###
+# Loads all of the scripts contained in the utils directory.
+#
+# This function will iterate through the utils directory in alphabetical order.
+# Each shell script ending in ".sh" will be loaded into the current session for
+# use.
+##
+if [ "$BASH_UTILS" != '' ]; then
+    if [ -d "$BASH_UTILS" ]; then
+        while read SCRIPT; do
+            source "$SCRIPT"
+        done < <(find "$BASH_UTILS/utils" -maxdepth 1 -name '*.sh')
+    else
+        echo "$BASH_UTILS: No such directory" >&2
+    fi
+else
+    echo "BASH_UTILS: Environment variable not defined" >&2
+fi
