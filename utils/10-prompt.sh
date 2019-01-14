06d1a2f542b0323be95875c3532b290c3f94e2da
Kevin Herrera <kevin@herrera.io>
2019-01-14 10:55:38 -0800

    Using PS1 directly and fixing line wrap issue.

--- utils/10-prompt.sh
+++ utils/10-prompt.sh
@@ -3,4 +3,4 @@
 # Please refer to this documentation on available BASH prompt options:
 # http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html
 
-export PROMPT_COMMAND='PS1="'"$(color -fdark-gray)"'[\t]'"$(color -r) $(color -fgreen)"'\W'"$(color -r) $(color -fmagenta)"'\$'"$(color -r)"' "'
+export PS1="\[$(color -fdark-gray)\][\t]\[$(color -r)\] \[$(color -fgreen)\]\W\[$(color -r)\] \[$(color -fmagenta)\]\$\[$(color -r)\] "
