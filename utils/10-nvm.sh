1410eccf2fa7d02717132ed4f18d8688185d7394
Kevin Herrera <kevin@herrera.io>
2019-01-14 20:24:12 -0800

    Only defining NVM_DIR if script exists.

--- utils/10-nvm.sh
+++ utils/10-nvm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export NVM_DIR="$HOME/.nvm"
+if [ -s "$HOME/.nvm/nvm.sh" ]; then
+    export NVM_DIR="$HOME/.nvm"
 
-if [ -s "$NVM_DIR/nvm.sh" ]; then
     source "$NVM_DIR/nvm.sh"
 fi
