68e3c5bd554be06e872dec1b2133c94190745922
Kevin Herrera <kevin@herrera.io>
2020-03-04 18:49:00 -0800

    Adding BASH completion for NVM.

--- utils/10-nvm.sh
+++ utils/10-nvm.sh
@@ -5,6 +5,10 @@ if [ -s "$HOME/.nvm/nvm.sh" ]; then
 
     source "$NVM_DIR/nvm.sh"
 
+    if [ -s "$NVM_DIR/bash_completion" ]; then
+        source "$NVM_DIR/bash_completion"
+    fi
+
     # Register as enabled.
     bu_enabled $(basename "$BASH_SOURCE")
 fi
