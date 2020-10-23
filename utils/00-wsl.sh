c3dcf9e6cc3557f6566463a9ec9c0ae06d887dc2
Kevin Herrera <kevin@herrera.io>
2020-10-23 15:24:09 -0700

    Supporting WSL2.

--- utils/00-wsl.sh
+++ utils/00-wsl.sh
@@ -1,15 +1,52 @@
 #!/usr/bin/env bash
 
-# Only run the script if inside WSL.
-if grep -i Microsoft /proc/version &> /dev/null; then
+# Check what version of WSL we are running in.
+if uname -a | grep -i microsoft > /dev/null; then
+    if [ -d /run/WSL ]; then
+        export WSL=2
+    else
+        export WSL=1
+    fi
+else
+    export WSL=0
+fi
+
+# Make changes only in WSL.
+if [ $WSL -gt 0 ]; then
 
     # Fix umask if its still an issue.
     if [ "$(umask)" = "0000" ]; then
         umask 022
     fi
 
-    # Configure a default display server.
-    export DISPLAY="localhost:0.0"
+    # Set the default display server.
+    if [ $WSL -eq 1 ]; then
+        export DISPLAY=":0.0"
+    else
+        export DISPLAY="$(grep nameserver /etc/resolv.conf | sed 's/nameserver //'):0.0"
+    fi
+
+    # Define a function to simplify launching GUI apps.
+    function gui
+    {
+        # Check usage.
+        if [ "$1" = '' ]; then
+            echo "Usage: gui COMMAND
+Launches a GUI application in the background.
+
+ARGUMENTS
+
+    COMMAND The command line arguments to launch the app.
+"
+            return 0
+        fi
+
+        # Create a log file.
+        local LOG="$(tempfile -d /tmp/gui)"
+
+        # Launch the app.
+        nohup "$@" &> "$LOG" &
+    }
 
     # Register as enabled.
     bu_enabled $(basename "$BASH_SOURCE")
