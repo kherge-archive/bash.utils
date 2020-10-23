8d09a96022762dfc6d57611aea21e5727b42da3e
Kevin Herrera <kevin@herrera.io>
2020-10-23 14:44:00 -0700

    Reverting all changes.

--- utils/00-wsl.sh
+++ utils/00-wsl.sh
@@ -2,17 +2,15 @@
 
 # Only run the script if inside WSL.
 if grep -i Microsoft /proc/version &> /dev/null; then
-    
+
     # Fix umask if its still an issue.
     if [ "$(umask)" = "0000" ]; then
         umask 022
     fi
 
     # Configure a default display server.
-    export HOST_IP="$(grep nameserver /etc/resolv.conf | sed 's/nameserver //')"
-    export DISPLAY="$HOST_IP:0.0"
+    export DISPLAY="localhost:0.0"
 
     # Register as enabled.
     bu_enabled $(basename "$BASH_SOURCE")
 fi
-
