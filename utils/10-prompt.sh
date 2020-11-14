d3a0efd3d1fa82c5ee5cb5d1f2805ec4f269384e
Kevin Herrera <kevin@herrera.io>
2020-11-13 21:05:23 -0800

    Using Starship if available.

--- utils/10-prompt.sh
+++ utils/10-prompt.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-# Please refer to this documentation on available BASH prompt options:
-# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html
+# Use starship if available.
+if command -v starship > /dev/null; then
 
-export PS1="\[$(color -fdark-gray)\][\t]\[$(color -r)\] \[$(color -fgreen)\]\W\[$(color -r)\] \[$(color -fmagenta)\]\$\[$(color -r)\] "
+    eval "$(starship init bash)"
+
+# Use hand-built PS1 prompt.
+else
+
+    # Please refer to this documentation on available BASH prompt options:
+    # http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html
+
+    export PS1="\[$(color -fdark-gray)\][\t]\[$(color -r)\] \[$(color -fgreen)\]\W\[$(color -r)\] \[$(color -fmagenta)\]\$\[$(color -r)\] "
+fi
 
 # Register as enabled.
 bu_enabled $(basename "$BASH_SOURCE")
